caf1f3251a055ac7fe3b38b987ba5075d18841fb
jesus-martin-eng <jesus.martin.eng@outlook.es>
2025-03-28 10:35:19 +0100

    modelo Espacio

--- zoo/models/models.py
+++ zoo/models/models.py
@@ -26,5 +26,19 @@ class Cuidador(models.Model):
     ], string='Cargo', required=True)
     fecha_incorporacion = fields.Datetime(string = "Fecha de Incorporación", required=True)
 
+class Espacio(models.Model):
+    _name = 'espacio.zoo'
+    _description = 'espacio.zoo'
 
+    codigo_espacio = fields.Char(string = "Código", required=True)
+    tipo = fields.Selection([('jaula', 'Jaula'),
+                             ('vallado', 'Vallado'),
+                             ('acuario', 'Acuario'),
+                             ('terrarium', 'Terrarium')], string = "Tipo", required=True)
+    area_tematica = fields.Selection([('europa', 'Europa'),
+                            ('asia', 'Asia'),
+                            ('africa', 'África'),
+                            ('america', 'América'),
+                            ('oceania', 'Oceanía')
+                            ], string='Área Temática', required=True)
 
