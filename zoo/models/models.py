a7ffe3dfca5603b6494bdb360310b0984348ba04
jesus-martin-eng <jesus.martin.eng@outlook.es>
2025-03-28 10:30:58 +0100

    Modelo Animal

--- zoo/models/models.py
+++ zoo/models/models.py
@@ -1,4 +1,17 @@
 # -*- coding: utf-8 -*-
 
+from odoo import models, fields, api
+
+class Animal(models.Model):
+    _name = 'animal.zoo'
+    _description = 'animal.zoo'
+
+    name = fields.Char()
+    especie = fields.Char()
+    identificador = fields.Char()
+    pais_procedencia = fields.Many2one('res.country', 'Pais', required=True)
+    edad = fields.Integer()
+    fecha_entrada = fields.Datetime()
+
 
 
